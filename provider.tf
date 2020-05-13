28195ba969edbb5b377e1f7c21669bcad2515205
Priyanshu Verma <priyanshuverma036@gmail.com>
2020-05-13 15:26:07 +0530

    DEM-1

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
+    credentials = "${file("./creds/credentials.json")}"
     project = "searce-playground"
     zone = "asia-south1"
 }
