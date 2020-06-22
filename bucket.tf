8c0cd93fc257bcc399652e64797d666037f0ee4e
Priyanshu Verma <priyanshuverma036@gmail.com>
2020-06-22 15:38:43 +0530

    modified bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,11 +1,11 @@
 # Specify the GCP Provider
 provider "google" {
 project = "project-apigate-stg-hub"
-region  = "asia-south1-a"
+region  = "asia-south1"
 }
 
 # Create a GCS Bucket
 resource "google_storage_bucket" "my_bucket" {
 name     = "testing-bucket"
-location = "asia-south1-a"
+location = "asia-south1"
 }
