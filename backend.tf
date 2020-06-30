fe69e421337ba77df44ef8fd29ed3db96318be17
Priyanshu <priyanshu.verma@searce.com>
2020-06-30 13:11:18 +0530

    modified Jenkinsfile

---
+++ backend.tf
@@ -0,0 +1,7 @@
+# file: backend.tf
+terraform {
+  backend "gcs" {
+    bucket = "apigate-searce-tf-bucket"
+    prefix = "stg/project-apigate-stg-hub/global/demo-bucket"
+  }
+}
--- bucket.tf
+++ bucket.tf
@@ -6,6 +6,6 @@ region  = "asia-south1"
 
 # Create a GCS Bucket
 resource "google_storage_bucket" "default" {
-name     = "apigate-testing-bucket"
+name     = "apigate-testing-bucket2"
 location = "asia-south1"
 }
